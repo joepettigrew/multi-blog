dacc3baaea49c38e8a56c37007f2680abb71bcf1
Joe Pettigrew <joepettigrew@joes-mbp-2.home>
2017-04-23 12:59:59 +0100

    Add login page

--- main.py
+++ main.py
@@ -226,6 +226,36 @@ class BlogSubmit(Handler):
             self.render("blogsubmit.html", title=title, content=content, error=error, username=username)
 
 
+class LogIn(Handler):
+    def get(self):
+        self.anom_page("login.html", "/welcome")
+
+    def post(self):
+        have_error = False
+        username = self.request.get("username")
+        password = self.request.get("password")
+
+        params = dict(username=username)
+
+        if self.valid_cred(username, password):
+            # Create cookie
+            self.set_secure_cookie("username", username)
+
+            # Redirect user to welcome page
+            self.redirect("/welcome")
+        else:
+            have_error = True
+            params['error_msg'] = "Invalid username or password"
+            self.render("login.html", **params)
+
+
+    def valid_cred(self, username, password, pass_hash=""):
+        query = db.GqlQuery("SELECT * FROM Users WHERE username = :1", username).get()
+        if query is not None:
+            pass_hash = query.password
+        return username and password and valid_pw(username, password, pass_hash)
+
+
 class LogOut(Handler):
     def get(self):
         self.logout()
@@ -237,5 +267,6 @@ app = webapp2.WSGIApplication([
     ('/signup', SignUpPage),
     ('/welcome', WelcomePage),
     ('/blogsubmit', BlogSubmit),
+    ('/login', LogIn),
     ('/logout', LogOut)
 ], debug=True)
